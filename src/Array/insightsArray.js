2a81bab6addd2185ecf2e39af974b8a1178ba128
unknown <castalonirenz@gmail.com>
2018-12-09 00:53:30 +0800

    12:53am

---
+++ src/Array/insightsArray.js
@@ -0,0 +1,23 @@
+export const insights = [
+    {
+        id: 0,
+        title: "Lorem ipsum dolor sit amet",
+        content: "Quisque a est vel tortor lobortis scelerisque vitae id risus.",
+        details: "SOLUTIONS",
+        date: "Nov 7, 2018"
+    },
+    {
+        id: 1,
+        title: "Lorem ipsum dolor sit amet",
+        content: "Quisque a est vel tortor lobortis scelerisque vitae id risus.",
+        details: "SOLUTIONS",
+        date: "Nov 7, 2018"
+    },
+    {
+        id: 2,
+        title: "Lorem ipsum dolor sit amet",
+        content: "Quisque a est vel tortor lobortis scelerisque vitae id risus.",
+        details: "SOLUTIONS",
+        date: "Nov 7, 2018"
+    }
+]
--- src/screens/Routes/Insights/Insights.js
+++ src/screens/Routes/Insights/Insights.js
@@ -1,8 +1,16 @@
-import { View, Text, TouchableOpacity, StyleSheet, Image, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  ScrollView
+} from "react-native";
 import React, { Component } from "react";
-import { Company } from "../../../Array/stringName"
+import { Company } from "../../../Array/stringName";
 import Icon from "react-native-vector-icons/Ionicons";
-import { MyTheme } from '../../../themes/globalTheme';
+import { MyTheme } from "../../../themes/globalTheme";
+import { insights } from "../../../Array/insightsArray";
 class Insights extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: Company.itemThree,
@@ -22,48 +30,63 @@ class Insights extends Component {
   });
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
   render() {
     return (
-      <View style={MyTheme.Container}>
-        <View style={styles.subContainer}>
-        <View style={styles.itemContainer}>
-        <Text> Insights </Text>
-        <Text> Insights </Text>
-        <Text> Insights </Text>
-        </View>
-        <View style={styles.imageHolder}>
-
-        </View>
-        </View>
-       
+      <ScrollView contentContainerStyle={{flexGrow:1}}>
+      <View style={{flex: 1, alignItems:"center"}}>
+        {insights.map((items, index) => (
+          <TouchableOpacity key={index} style={styles.subContainer}>
+            <View style={styles.itemContainer}>
+              <Text 
+              numberOfLines={1}
+              style={MyTheme.headerText}> {items.title} </Text>
+              <Text 
+              numberOfLines={2}
+              style={MyTheme.textContent}> {items.content} </Text>
+              <View style={{flexDirection:"row"}}>
+                <Text>{items.details}</Text>
+                <Text> {items.date} </Text>
+              </View>
+            </View>
+            <View style={styles.imageHolder}>
+            <Text>Image</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
       </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  subContainer:{
-    alignItems: 'center',
-    width:"90%",
-    backgroundColor:"orange",
-    flexDirection: 'row',
+  subContainer: {
+    alignItems: "center",
+    width: "90%",
     justifyContent: 'space-between',
-    // paddingLeft: 20,
-    // paddingRight: 20,
+    flexDirection: "row",
+    marginTop: 20,
+    borderBottomColor: "#D8D8D8" ,
+    borderBottomWidth: 1,
+    paddingBottom: 5,
+    paddingRight: 20,
+    
   },
-  itemContainer:{
-    backgroundColor:"pink"
+  itemContainer: {
+    width: "80%",
+  
+    
   },
-  imageHolder:{
-    height: 80,
+  imageHolder: {
+    height: "90%",
     width: 80,
-    backgroundColor:"blue"
-
+    backgroundColor: "#D8D8D8",
+    
   }
-
-})
+});
 export default Insights;
+
+    
